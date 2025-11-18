91c2e8338541e58d793cda25c389ec17a6848a0e
Phann Pha <phannpha.fa@gmail.com>
2025-11-18 11:25:56 +0700

    Testing commit changed

--- IncomeApp/ContentView.swift
+++ IncomeApp/ContentView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Text("Hello world!")
+                .font(.system(size: 16, weight: .regular))
         }
         .padding()
     }
