a422c661b932bb822d980098ab105cebf4389915
Phann Pha <phannpha.fa@gmail.com>
2025-11-18 10:45:24 +0700

    Initial Commit

---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by Pha on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
