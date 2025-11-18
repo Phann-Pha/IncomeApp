f648b368bbacf05d1c6a98fc5cfd4da8d8dc9844
Phann Pha <phannpha.fa@gmail.com>
2025-11-18 12:09:51 +0700

    first committed to development branch

--- IncomeApp/ContentView.swift
+++ IncomeApp/ContentView.swift
@@ -3,8 +3,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello world!")
+            Text("Get Started")
                 .font(.system(size: 16, weight: .regular))
+                .padding(10)
+                .foregroundStyle(.white)
+                .background(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+                
         }
         .padding()
     }
